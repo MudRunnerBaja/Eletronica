2d60179fcc7e3af81c0c9a2d2a4c1f6a59a55b42
lucasbndv <57725673+lucasbndv@users.noreply.github.com>
2020-05-12 20:13:41 -0300

    Colocando a comunicação com o sensor rf
    Funcionando a lógica de largada, 30m e retomada

--- AceleracaoRetomada/30m_A___R.ino
+++ AceleracaoRetomada/30m_A___R.ino
@@ -1,22 +1,85 @@
-int sensor30 = 6;  
-
-void setup() { 
-
-  // put your setup code here, to run once: 
-
-  pinMode(sensor30, INPUT_PULLUP); 
-  Serial.begin(9600); 
-
-} 
-
-void loop() { 
-
-  // Código para pegar tempo de aceleração: 
-
-    //Serial.println("Estado do sensor_30m:");
-    if(digitalRead(sensor30) == 0){ 
-      Serial.println('@');
-      } 
+#include<SoftwareSerial.h>
+int sensor30;
+int sensorfinal = 6;
+int vel = 0;
+int rpm = 0;
+float tempoant = 0;
+float tempoace = 0;
+float tempofinal = 0;
+float tempoinicial = 0;
+bool contado30 = false;
+bool contadoinicio = false;
+bool inicio = true;
+bool final = false;
+bool recebevel = false;
+bool receberpm = false;
+bool mostrarvel = false;
 
 
-} 
+SoftwareSerial m30(10, 11);
+
+void setup() {
+
+  // put your setup code here, to run once:
+
+  pinMode(sensorfinal, INPUT_PULLUP);
+
+  m30.begin(9600);
+  Serial.begin(9600);
+
+}
+
+
+
+void loop() {
+
+  // Código para pegar tempo de aceleração:
+  if (mostrarvel == true) {
+    Serial.print(vel);
+    mostrarvel = false;
+  }
+
+  if (m30.available()) {
+    sensor30 = m30.read();
+  }
+
+  if (sensor30 == '@' && contado30 == false) {
+
+    tempoace = millis();
+    tempoant = millis();
+    Serial.println("O carro acabou de passar aqui!\n");
+    Serial.print("Tempo 30 metros: ");
+    Serial.println(tempoace / 1000);
+    contado30 = true;
+
+  }
+
+
+
+  // Código para passagem final:
+
+  if (digitalRead(sensorfinal) == 0) {
+
+    if (inicio) {
+
+      tempoinicial = millis();
+      inicio = false;
+      Serial.println("Iniciado o teste");
+
+    }
+
+
+
+    if (contado30 == true && final == false) {
+
+      tempofinal = millis() - tempoinicial;
+      Serial.println("\nTeste finalizado!\n");
+      Serial.print("Tempo final: ");
+      Serial.println((tempofinal + tempoace) / 1000);
+      final = true;
+
+    }
+
+  }
+
+}
