e037f91f0eea749e8579c6ac1c41d357ed44f561
lucasbndv <57725673+lucasbndv@users.noreply.github.com>
2020-05-12 20:14:52 -0300

    Adicionada comunicação com sensor rf
    Lógica de largada,30m e retomada feitas

--- AceleracaoRetomada/Principal_A_R.ino
+++ AceleracaoRetomada/Principal_A_R.ino
@@ -1,76 +1,85 @@
+#include<SoftwareSerial.h>
 int sensor30;
-int sensorfinal = 6; 
-float tempoant = 0; 
-float tempoace = 0; 
-float tempofinal = 0; 
-float tempoinicial = 0; 
-bool contado30 = false; 
-bool contadoinicio = false; 
-bool inicio = true; 
-bool final = false; 
+int sensorfinal = 6;
+int vel = 0;
+int rpm = 0;
+float tempoant = 0;
+float tempoace = 0;
+float tempofinal = 0;
+float tempoinicial = 0;
+bool contado30 = false;
+bool contadoinicio = false;
+bool inicio = true;
+bool final = false;
+bool recebevel = false;
+bool receberpm = false;
+bool mostrarvel = false;
 
-  
 
-void setup() { 
+SoftwareSerial m30(10, 11);
 
-  // put your setup code here, to run once: 
- 
-  pinMode(sensorfinal, INPUT_PULLUP); 
-  Serial.begin(9600); 
+void setup() {
 
-} 
+  // put your setup code here, to run once:
 
-  
+  pinMode(sensorfinal, INPUT_PULLUP);
 
-void loop() { 
+  m30.begin(9600);
+  Serial.begin(9600);
 
-  // Código para pegar tempo de aceleração: 
+}
 
-if (Serial.available()) { 
 
-// atribuindo o valor do serial.read para o sensor30 
 
- sensor30 = Serial.read(); 
+void loop() {
 
-} 
+  // Código para pegar tempo de aceleração:
+  if (mostrarvel == true) {
+    Serial.print(vel);
+    mostrarvel = false;
+  }
 
-  if (sensor30 == '@' && contado30 == false) { 
+  if (m30.available()) {
+    sensor30 = m30.read();
+  }
 
-    tempoace = millis(); 
-    tempoant = millis(); 
-    Serial.println("O carro acabou de passar aqui!\n"); 
-    Serial.print("Tempo 30 metros: "); 
-    Serial.println(tempoace / 1000); 
-    contado30 = true; 
+  if (sensor30 == '@' && contado30 == false) {
+
+    tempoace = millis();
+    tempoant = millis();
+    Serial.println("O carro acabou de passar aqui!\n");
+    Serial.print("Tempo 30 metros: ");
+    Serial.println(tempoace / 1000);
+    contado30 = true;
 
   }
 
-  
 
-  // Código para passagem final: 
 
-  if (digitalRead(sensorfinal) == 0) { 
+  // Código para passagem final:
 
-    if (inicio) { 
+  if (digitalRead(sensorfinal) == 0) {
 
-      tempoinicial = millis(); 
-      inicio = false; 
-      Serial.println("Iniciado o teste"); 
+    if (inicio) {
 
-    } 
+      tempoinicial = millis();
+      inicio = false;
+      Serial.println("Iniciado o teste");
 
-  
+    }
 
-    if (contado30 == true && final == false) { 
 
-      tempofinal = millis() - tempoinicial; 
-      Serial.println("\nTeste finalizado!\n"); 
-      Serial.print("Tempo final: "); 
-      Serial.println((tempofinal + tempoace)/1000); 
-      final = true; 
 
-    } 
+    if (contado30 == true && final == false) {
 
-  } 
+      tempofinal = millis() - tempoinicial;
+      Serial.println("\nTeste finalizado!\n");
+      Serial.print("Tempo final: ");
+      Serial.println((tempofinal + tempoace) / 1000);
+      final = true;
 
-} 
+    }
+
+  }
+
+}
