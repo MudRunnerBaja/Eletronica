ec8300b0eb6149c096eff0ad1a292f428c1b0f14
Favoreto <57758959+MudRunnerBaja@users.noreply.github.com>
2020-05-20 20:23:12 -0300

    simulacao

---
+++ AceleracaoRetomada/30metros.ino
@@ -0,0 +1,22 @@
+int sensor30 = 6;  
+
+void setup() { 
+
+  // put your setup code here, to run once: 
+
+  pinMode(sensor30, INPUT_PULLUP); 
+  Serial.begin(9600); 
+
+} 
+
+void loop() { 
+
+  // Código para pegar tempo de aceleração: 
+
+    //Serial.println("Estado do sensor_30m:");
+    if(!digitalRead(sensor30)){ 
+      Serial.println('1');
+      } 
+
+
+}
--- AceleracaoRetomada/Principal_A_R.ino
+++ AceleracaoRetomada/Principal_A_R.ino
@@ -1,79 +1,48 @@
 #include<SoftwareSerial.h>
-int sensor30;
-int sensorfinal = 6;
-int vel = 0;
-int rpm = 0;
-char data;
-float tempoant = 0;
-float tempoace = 0;
-float tempofinal = 0;
-float tempoinicial = 0;
-bool contado30 = false;
-bool contadoinicio = false;
-bool inicio = true;
-bool final = false;
-bool lendorpm = false;
 
-//Declarando as comunicações
-SoftwareSerial ace(10, 11);
-SoftwareSerial carro(8, 9);
+SoftwareSerial mySerial(10, 11);
+
+#define button 6
+
+bool inicio = false, trintaMetros = false;
+float tempoInicial, tempoTotal, tempo30Metros;
 
 void setup() {
-  pinMode(sensorfinal, INPUT_PULLUP);
 
-  ace.begin(9600);
+  // put your setup code here, to run once:
+
+  pinMode(button, INPUT_PULLUP);
+  mySerial.begin(38400);
+  mySerial.println("qualquermerda");
+
   Serial.begin(9600);
 
 }
 
+
+
 void loop() {
-  //Recebendo 30m sensor:
-  ace.listen();
-  while (ace.available()) {
-    sensor30 = ace.read();
+
+  if((!digitalRead(button)) and (!inicio)){
+    mySerial.println("Iniciado o teste");
+    tempoInicial = millis();
+    inicio = true;
   }
-
-  //Recebendo velocidade e rpm:
-  carro.listen();
-  while (carro.available()) {
-    data = carro.read();
-    if (data == '!' || lendorpm == true) {
-      rpm = carro.read();
-      lendorpm = true;
-    }
-
-    else {
-      vel = carro.read();
-    }
-  }
-  lendorpm = false;
-
-  if (sensor30 == '@' && contado30 == false) {
-
-    tempoace = millis();
-    tempoant = millis();
-    Serial.println("O carro acabou de passar aqui!\n");
-    Serial.print("Tempo 30 metros: ");
-    Serial.println(tempoace / 1000);
-    contado30 = true;
-  }
-
-  // Código para passagem final:
-
-  if (digitalRead(sensorfinal) == 0) {
-    
-    if (inicio) {
-      tempoinicial = millis();
-      inicio = false;
-      Serial.println("Iniciado o teste");
-    }
-
-    if (contado30 == true && final == false) {
-      tempofinal = millis() - tempoinicial;
-      Serial.println("\nTeste finalizado!\n");
-      Serial.print("Tempo final: ");
-      Serial.println((tempofinal + tempoace) / 1000);
-      final = true;
-    }
-  }
+  if(inicio){
+    if((Serial.read()=='1') and (!trintaMetros)){
+      tempo30Metros = millis() - tempoInicial;
+      mySerial.println("30 metros alcancados!");
+      mySerial.print("Tempo percorrido:");
+      mySerial.println(tempo30Metros/1000);
+      trintaMetros = true;
+     }
+    if((trintaMetros) and (!digitalRead(button))){
+      tempoTotal = millis() - tempoInicial;
+      mySerial.println("Fim do teste");
+      mySerial.print("Tempo total:");
+      mySerial.println(tempoTotal/1000);
+      trintaMetros = false; inicio = false;
+      delay(500);
+      } 
+  } 
 }
