a8e42b2ee6f139d0b0f089f6ee70a69120871012
lucasbndv <57725673+lucasbndv@users.noreply.github.com>
2021-07-12 13:45:39 -0300

    Adicionado o arquivo de 30m

---
+++ testeAceleracaoRetomada/30m.ino
@@ -0,0 +1,21 @@
+long debouncing_time = 100; //Debouncing Time in Milliseconds
+volatile unsigned long last_micros;
+int ir_sensor = 3;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ir_sensor,INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(ir_sensor), interruptFunc, FALLING);
+}
+
+void loop() {
+
+}
+
+void interruptFunc() {
+  if ((long)(micros() - last_micros) >= debouncing_time * 1000)
+  {
+    Serial.print('!');
+    last_micros = micros();
+  }
+} 
