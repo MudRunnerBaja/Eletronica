9fdd84f29ce7016ff1c3eb33c9602470670f5518
RafoDias <rhedias@hotmail.com>
2022-10-24 14:02:33 -0300

    Reorganização

---
+++ codigos/Competição/RaspbabyPico/main/include/display.c
@@ -0,0 +1,110 @@
+#define A 5
+#define B 6
+#define C 7
+#define D 10
+#define E 11
+#define F_DISPLAY 12
+#define G 13
+
+byte cont = 0;
+int displays[] = {14, 15, 16, 17};
+
+const byte tabela_7seg[10][7] =
+    {
+        // A B C D E F G
+        {0, 0, 0, 0, 0, 0, 1}, // 0
+        {1, 0, 0, 1, 1, 1, 1}, // 1
+        {0, 0, 1, 0, 0, 1, 0}, // 2
+        {0, 0, 0, 0, 1, 1, 0}, // 3
+        {1, 0, 0, 1, 1, 0, 0}, // 4
+        {0, 1, 0, 0, 1, 0, 0}, // 5
+        {0, 1, 0, 0, 0, 0, 0}, // 6
+        {0, 0, 0, 1, 1, 1, 1}, // 7
+        {0, 0, 0, 0, 0, 0, 0}, // 8
+        {0, 0, 0, 0, 1, 0, 0}  // 9
+};
+
+void escolherDisplay(int contador)
+{
+    int i = 0;
+    for (i = 0; i < 4; i++)
+    {
+        if (i == contador)
+            digitalWrite(displays[contador], HIGH);
+        else
+            digitalWrite(displays[i], LOW);
+    }
+}
+
+void converteValorDisplay(int valor)
+{
+    int pino = 5;
+    valor = valor > 9 ? 9 : valor;
+
+    for (int x = 0; x < 7; x++)
+    {
+        digitalWrite(pino, tabela_7seg[valor][x]);
+        if (pino == 7)
+            pino = 10;
+        else
+            pino++;
+    }
+}
+
+void displaySetup()
+{
+  for (int i = 0; i < 4; i++)
+  {
+    pinMode(displays[i], OUTPUT);
+    digitalWrite(displays[i], LOW);
+  }
+  pinMode(DS, OUTPUT);
+  pinMode(STCP, OUTPUT);
+  pinMode(SHCP, OUTPUT);
+  digitalWrite(STCP, LOW);
+  digitalWrite(SHCP, LOW);
+  digitalWrite(DS, LOW);
+
+  for (int i = 0; i < 4; i++)
+   {
+    // Serial.println("\nMostrando dados...");
+    // Serial.print("Valor: "); Serial.println(valores[i]);
+    setLedBox();
+    escolherDisplay(i);
+    escreverRegistrador(valores[i]);
+    delay(500);
+   }
+}
+
+void mostraDados()
+{
+    int contaDisplay = 0;
+    float vel = getVelValue();
+    int rpm = getRpmValue();
+
+    // RPM
+    int milharRPM = rpm / 1000;
+    escolherDisplay(contaDisplay);
+    converteValorDisplay(milharRPM);
+    contaDisplay++;
+    delay(1);
+
+    int centenasRPM = (rpm % 1000) / 100;
+    escolherDisplay(contaDisplay);
+    converteValorDisplay(centenasRPM);
+    contaDisplay++;
+    delay(1);
+
+    // Velocidade
+    int dezenasVel = vel / 10;
+    escolherDisplay(contaDisplay);
+    converteValorDisplay(dezenasVel);
+    contaDisplay++;
+    delay(1);
+
+    int unidadesVel = int(vel) % 10;
+    escolherDisplay(contaDisplay);
+    converteValorDisplay(unidadesVel);
+    contaDisplay++;
+    delay(1);
+}
---
+++ codigos/Competição/RaspbabyPico/main/main.ino
@@ -0,0 +1,73 @@
+#include <RPi_Pico_TimerInterrupt.h> // Interrupção por tempo
+#include <RPi_Pico_ISR_Timer.h>
+#include <RPi_Pico_ISR_Timer.hpp>
+
+#include "include/temp.c" // Temperatura CVT
+#include "include/comb.c" // Níveis de combustível
+#include "include/vel.c" // Velocidade do carro
+#include "include/rpm.c" // RPM do carro
+#include "include/display.c" // Placa do display
+#include "include/sdcard.c"
+#include "include/comunication.c" // Comunicação entre bibliotecas e serial
+
+
+#define TIMER_INTERVAL_MS 1000
+
+RPI_PICO_Timer ITimer(0);
+
+// Interrupt callback functions
+bool TimerHandler(struct repeating_timer *t)
+{
+  sendData();
+  return true;
+}
+
+void setup()
+{
+  Serial.begin(9600);
+
+      // Esperando pela resposta do monitor serial. 
+      // Comentar quando for para o carro.
+  /*
+  while (!Serial) {
+    yield();
+  }
+  delay(1000);
+
+  Serial.println(F("Type any character to start"));
+  while (!Serial.available()) {
+    yield();
+  }
+  */
+  
+  Serial.println("Iniciando setup...");
+  // setupGps();
+  combSetup();
+  displaySetup();
+  cvtSetup();
+  velSetup();
+  rpmSetup();
+  sdcardSetup();
+    
+  delay(1000);
+  digitalWrite(ledTempCvt, LOW);
+  digitalWrite(ledVerde, LOW);
+  digitalWrite(ledAmarelo, LOW);
+  digitalWrite(ledVermelho, LOW);
+    
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  // Interval in unsigned long microseconds
+  if (ITimer.attachInterruptInterval(TIMER_INTERVAL_MS * 1000, TimerHandler))
+    Serial.println("Starting ITimer OK, millis() = " + String(millis()));
+  else
+    Serial.println("Can't set ITimer. Select another freq. or timer");
+
+    Serial.print("Setup finalizado.");
+}
+void loop()
+{
+  // updateGps(); // ainda não implementado no receptor
+  mostraDados();
+}
