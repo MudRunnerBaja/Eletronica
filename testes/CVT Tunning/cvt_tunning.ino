d129118306cdc819aa97b26f394d37c63d07384b
Lucas Coelho <lucas.berton7@gmail.com>
2023-01-19 02:17:04 -0300

    Implementando o GPS no codigo do CVT Tunning

--- testes/CVT Tunning/cvt_tunning.ino
+++ testes/CVT Tunning/cvt_tunning.ino
@@ -3,6 +3,8 @@
 #include <SD.h>
 #include <Arduino.h>
 #include <LiquidCrystal.h>
+#include <SoftwareSerial.h>
+#include <TinyGPS.h>
 
 /*
 Variaveis e funções "1" são referentes a motriz, variaveis e funcões "2" são referente a movida e variaveis e funções "3" são referentes a velocidade
@@ -11,20 +13,20 @@ rpm1 = rpm da motriz / rpm2 = rpm da movida
 contador1 = contador da motriz / contador2 = contador da movida
 */
 File myFile;
-Ds1302 rtc(28, 31, 30); // 28 - RST ; 30 - DAT ; 31 - CLK
+TinyGPS gps;
+
+SoftwareSerial ss(30, 31);
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
 
 int rpm1, rpm2, velo;
-float pulsohora;
-int hall1, hall2, hall3, falha, chipSelect = 53;
+int hall1, hall2, falha, chipSelect = 53;
 String arq = "cvt_00.csv";
-volatile byte pulsos1, pulsos2, pulsos3, pulsomin;
-unsigned long timeold, timeold1;
-unsigned int pulsos_por_volta_mtz = 1;            //Quantidade de imas na polia
-unsigned int pulsos_por_volta_mvd = 4;
-unsigned int pulsos_por_volta_velo = 4;       //Quantidade de imas no disco de freio
-const float circunferencia_pneu = 167.4876; //EM CM
+volatile byte pulsos1, pulsos2;
+unsigned long timeold;
+unsigned int pulsos_por_volta_mtz = 1;            
+unsigned int pulsos_por_volta_mvd = 4;          //Quantidade de imas na polia
+
 
 //REFRESH RATE EM ms
 const int refresh_rate = 250; //ms
@@ -37,9 +39,6 @@ void contador2(){                             //Contador de pulsos da movida
     pulsos2++;
 }
 
-void contador3(){                            //Contador de pulsos da velocidade
-    pulsos3++;
-}
 
 void error(){                               //Vai emitir um sinal no led da placa de acordo com o codigo do erro
   switch (falha)
@@ -80,6 +79,7 @@ void error(){                               //Vai emitir um sinal no led da plac
 void criarArquivo(){                        //Função para criar o arquivo e verificar o nome dele, caso o nome do arquivo ja exista
     int i = 0;                              //ele ira aumentar um numero no nome do arquivo, e ira verificar de novo, até que não haja um arquivo do mesmo nome
     char unidade;
+
     while (SD.exists(arq) == 1)
     {
         i++;
@@ -109,20 +109,11 @@ void criarArquivo(){                        //Função para criar o arquivo e ve
           arq[5] = y[0];
         }
     }
+
     Serial.print("Arquivo ");
   	Serial.print(arq);
   	Serial.println(" criado");
-    Ds1302::DateTime now;
-    /*//                    FUNCÃO PARA SETAR O HORARIO
-    now.year = 22;
-    now.month = 8;
-    now.day = 31;
-    now.hour = 18;
-    now.minute = 1;
-    now.second = 0;    
-    rtc.setDateTime(&now);
-    */
-    rtc.getDateTime(&now);
+
     File myFile = SD.open(arq, FILE_WRITE);
     if (myFile){
     }else{
@@ -130,12 +121,7 @@ void criarArquivo(){                        //Função para criar o arquivo e ve
       falha = 1;
       error();
     }
-    myFile.print("Data ");
-    myFile.print(now.day);
-    myFile.print("/");
-    myFile.print(now.month);
-    myFile.print("/20");
-    myFile.print(now.year);
+
     myFile.print(",RPM Motriz,RPM Movida,km/h, refresh rate: ");
     myFile.print(refresh_rate);
     myFile.print("ms");
@@ -144,51 +130,37 @@ void criarArquivo(){                        //Função para criar o arquivo e ve
 
     lcd.clear();
     lcd.print(arq);
-    lcd.setCursor(0,2);
-    lcd.print(now.day);
-    lcd.write("/");
-    lcd.print(now.month);
-    lcd.write(" ");
-    lcd.print(now.hour);
-    lcd.write(":");
-    lcd.print(now.minute);
-    lcd.write(":");
-    lcd.print(now.second);
     delay(3000);
 }
 
 void setup()
 {
   Serial.begin(115200);
+  ss.begin(4800);
   lcd.begin(16,2);
+
   Serial.println("Inicializando...");
   lcd.clear();
   lcd.write("CVT Tunning");
   Serial.println("CVT Tunning V 1.3.1");
   delay(500);
+
   pinMode(LED_BUILTIN, OUTPUT);
+
   if (!SD.begin(chipSelect)) {
     Serial.println("Inicialização do cartão falhou");
     falha = 0;
     error();
   }
-  rtc.init();
+
   criarArquivo();
   delay(1500);  
   hall1 = 19;                                // Define que o sensor hall da motriz sera ligado na porta 19
   hall2 = 20;                                // Define que o sensor hall da movida sera ligado na porta 20
-  hall3 = 21;                                // Define que o sensor hall da velocidade sera ligado na porta 21
-  pulsos1 = 0;
-  pulsos2 = 0;
-  pulsos3 = 0;
-  rpm1 = 0;
-  rpm2 = 0;
-  velo = 0;
-  timeold = 0;
-  timeold1 = 0;
+
   attachInterrupt(digitalPinToInterrupt(hall1), contador1, RISING);
   attachInterrupt(digitalPinToInterrupt(hall2), contador2, RISING);
-  attachInterrupt(digitalPinToInterrupt(hall3), contador3, RISING);
+
   Serial.println("Finalizada a inicalizacao");
 }
 
@@ -200,26 +172,17 @@ void loop()
     detachInterrupt(digitalPinToInterrupt(hall1));
     detachInterrupt(digitalPinToInterrupt(hall2));
 
+    velo = gps.f_speed_kmph();
+
     digitalWrite(LED_BUILTIN, HIGH);
 
     rpm1 = (60 * 1000 / pulsos_por_volta_mtz ) / (millis() - timeold) * pulsos1;
     rpm2 = (60 * 1000 / pulsos_por_volta_mvd ) / (millis() - timeold) * pulsos2;
-    
-
-    Ds1302::DateTime now;                   //Coleta os dados do rtc      
-    rtc.getDateTime(&now);
 
     timeold = millis();
     pulsos1 = 0;
     pulsos2 = 0;
-    
-
-    Serial.print(now.hour);                 //Imprime o horario no serial
-    Serial.print(":");
-    Serial.print(now.minute);
-    Serial.print(":");
-    Serial.print(now.second);
-    Serial.println();
+  
     Serial.print("RPM Motriz = ");          //Imprime os dados de rpm no serial
     Serial.println(rpm1, DEC);
     Serial.print("RPM Movida = ");
@@ -235,12 +198,7 @@ void loop()
       falha = 1;
       error();
     }
-    myFile.print(now.hour);
-    myFile.print(":");
-    myFile.print(now.minute);
-    myFile.print(":");
-    myFile.print(now.second);
-    myFile.print(",");
+
     myFile.print(rpm1);
     myFile.print(",");
     myFile.print(rpm2);
@@ -258,17 +216,5 @@ void loop()
 
     attachInterrupt(digitalPinToInterrupt(hall1), contador1, RISING);
     attachInterrupt(digitalPinToInterrupt(hall2), contador2, RISING);
-  }
-
-  if (millis() - timeold1 >= 1000)
-  {
-    detachInterrupt(digitalPinToInterrupt(hall3)); 
-
-    pulsomin = (60 * 1000 / pulsos_por_volta_velo ) / (millis() - timeold1) * pulsos3;
-    pulsohora = pulsomin * 60;
-    velo = (circunferencia_pneu / 100000) * pulsohora;                                    //transforma pulsos por hora em km/h
-    timeold1 = millis();
-    pulsos3 = 0; 
-    attachInterrupt(digitalPinToInterrupt(hall3), contador3, RISING);
   }
 }
