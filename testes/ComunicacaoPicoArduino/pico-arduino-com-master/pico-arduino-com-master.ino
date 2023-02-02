373e8389338f6509f3707935b50180fa4756fd57
RafoDias <rhedias@hotmail.com>
2023-02-02 18:18:44 -0300

    Improving communaction and LCD display

--- testes/ComunicacaoPicoArduino/pico-arduino-com-master/pico-arduino-com-master.ino
+++ testes/ComunicacaoPicoArduino/pico-arduino-com-master/pico-arduino-com-master.ino
@@ -5,7 +5,12 @@ int SDApin = 8;
 int SCLpin = 9;
 
 int counter = 0;
-byte testvel = 0;
+int testvel = 0;
+int testrpm = 0;
+
+byte data[4];
+
+int ld = LOW;
 
 unsigned int tf, t0, t1;
 
@@ -14,6 +19,9 @@ void setup() {
   Wire.setSDA(SDApin);
   Wire.setSCL(SCLpin);
   Wire.begin();
+
+  pinMode(21, OUTPUT);
+  pinMode(20, OUTPUT);
 
   Serial.begin(9600);
   tf = t0 = t1 = millis();
@@ -26,19 +34,31 @@ void loop() {
   if (tf - t1 >= 125)
   {
     testvel++;
-    if (testvel > 99) { testvel = 0; }
+    testrpm += 100;
+    if (testvel > 99) { testvel = 0;}
+    if (testrpm > 6000) { testrpm = 0;}
     Serial.print("\ntestvel: "); Serial.println(testvel);
+    Serial.print("\ntestrpm: "); Serial.println(testrpm);
     t1 = millis();
   }
 
   if (tf - t0 >= 250){
     unsigned int tmp = millis();
-    Serial.println("Transfering");
+
+    data[0] = highByte(testvel);
+    data[1] = lowByte(testvel);
+    data[2] = highByte(testrpm);
+    data[3] = lowByte(testrpm);
+    
     Wire.beginTransmission(UnoLCD);
-    Wire.write(testvel);
+    int c = Wire.write(data, 4);
     Wire.endTransmission();
+    
     tmp = millis() - tmp;
-    Serial.print("Done in ");Serial.println(tmp);
+    ld = !ld;
+    digitalWrite(21, ld);
+    digitalWrite(20, ld);
+    Serial.print(c); Serial.print(" bytes done in");Serial.println(tmp);
     t0 = millis();
   }
 }
