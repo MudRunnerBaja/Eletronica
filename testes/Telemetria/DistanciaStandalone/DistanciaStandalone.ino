13971d123e5f2ffe17ba4a9db53e86aeaf57ece8
Lucas Coelho <lucas.berton7@gmail.com>
2023-07-05 23:09:14 -0300

    Update DistanciaStandalone.ino

--- testes/Telemetria/DistanciaStandalone/DistanciaStandalone.ino
+++ testes/Telemetria/DistanciaStandalone/DistanciaStandalone.ino
@@ -1,12 +1,11 @@
-#include <SoftwareSerial.h>
 #include <LiquidCrystal.h>
 
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
 long timer, timer2;
-int counter;
 int data = 0;
 
+String sentData = "TESTETESTE123456789123456789TESTETESTE"; //Dado a ser enviado ao hc12
 int REFRESH_DISPLAY = 100; //Tempo em ms
 int POLLING_RATE_DATA = 1000; //Tempo em ms
 
@@ -17,37 +16,38 @@ void setup() {
 }
 
 void loop() {
-  hc12Read();
-  hc12Send();
-  lcdRefresh();
+  int bytesRead = hc12Read();
+  int bytesSent = hc12Send();
+  lcdRefresh(bytesRead, bytesSent);
 }
 
 
-void lcdRefresh(){
+void lcdRefresh(int bytesRead, int bytesSent){
   if(millis()>timer2){
-    lcd.print("Recived:");
-    lcd.write(data);
+    Serial.print("Recived:");
+    Serial.println(bytesRead);//
     lcd.setCursor(0,1);
-    lcd.print("Sent:");
-    lcd.print(counter);
+    Serial.print("Sent:");
+    Serial.println(bytesSent);//
 
-    timer2 = millis()+REFRESH_DISPLAY;
+    timer2 = millis()+1000;
   }
 }
 
-int hc12Read(){
-  while(Serial1.available()){
+int hc12Read(){                   //RETORNA QUANTIDADE DE BITS QUE FOI ENVIADO
+  int bytesRead = 0;
+  while(Serial1.available()>0){
     data = Serial1.read();
-    Serial.write(data);
+    bytesRead++;
   }
+  return sizeof(data);
 }
 
 
-int hc12Send(){
+int hc12Send(){                   //RETORNA A QUANTIDADE DE BITS QUE FOI RECIBIDA
   if(millis()>timer){
-    Serial1.println(counter);
-    counter++;
-
-    timer = millis() + POLLING_RATE_DATA;
+    Serial1.println(sentData);
+    timer = millis() + 1000;
   }
+  return  sizeof(sentData);
 }
