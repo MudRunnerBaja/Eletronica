881e28223f5902309ea28c7ae20ef78b4bf3d349
Lucas Coelho <lucas.berton7@gmail.com>
2023-07-05 22:00:51 -0300

    Update DistanciaStandalone.ino

--- testes/Telemetria/DistanciaStandalone/DistanciaStandalone.ino
+++ testes/Telemetria/DistanciaStandalone/DistanciaStandalone.ino
@@ -4,50 +4,50 @@
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
 long timer, timer2;
-int i;
+int counter;
 int data = 0;
-char dado[10];
 
+int REFRESH_DISPLAY = 100; //Tempo em ms
+int POLLING_RATE_DATA = 1000; //Tempo em ms
 
 void setup() {
-  Serial.begin(9600);             // Serial port to computer
-  Serial1.begin(9600);   // Serial port to Serial1
+  Serial.begin(9600);             // Porta serial para o arduino
+  Serial1.begin(9600);            // Porta serial para o HC12
   lcd.begin(16,2);
-  //Serial1.println("Testing HC-12 module range...");
 }
-  int p = 0;
+
 void loop() {
-  int newdata = 0;
+  hc12Read();
+  hc12Send();
+  lcdRefresh();
+}
 
-  // Wait for one second before sending the next message
-  
-  if(Serial1.available()){
-    lcd.clear();
-    newdata = 0;
+
+void lcdRefresh(){
+  if(millis()>timer2){
+    lcd.print("Recived:");
+    lcd.write(data);
+    lcd.setCursor(0,1);
+    lcd.print("Sent:");
+    lcd.print(counter);
+
+    timer2 = millis()+REFRESH_DISPLAY;
   }
+}
 
+int hc12Read(){
   while(Serial1.available()){
     data = Serial1.read();
     Serial.write(data);
-    lcd.write(data);
   }
-  
+}
 
- 
-  
-  if(millis()>timer2){
-    //lcd.print("Recived:");
-    lcd.setCursor(0,1);
-    lcd.print("Sent:");
-    lcd.print(i);
-    
-    timer2 = millis()+100;
-  }
-  
+
+int hc12Send(){
   if(millis()>timer){
-    Serial1.println(i);
-    timer = millis() + 1000;
-    i++;
-    newdata=1;
+    Serial1.println(counter);
+    counter++;
+
+    timer = millis() + POLLING_RATE_DATA;
   }
 }
