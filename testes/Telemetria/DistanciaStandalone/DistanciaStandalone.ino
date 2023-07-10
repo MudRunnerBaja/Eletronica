cd78857c9259d5d699e64d563db212f5da3ef821
Lucas <lucas.berton7@gmail.com>
2023-07-10 17:31:57 -0300

    Working ArdMega2560 B9600 Serial 2tx Serial 1rx

--- testes/Telemetria/DistanciaStandalone/DistanciaStandalone.ino
+++ testes/Telemetria/DistanciaStandalone/DistanciaStandalone.ino
@@ -2,61 +2,67 @@
 
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
-String sentData = "Testes"; //Dado a ser enviado ao hc12
+byte sentData = 0xFF; //Dado a ser enviado ao hc12
 int REFRESH_DISPLAY = 100; //Tempo em ms
 int POLLING_RATE_DATA = 1000; //Tempo em ms
+unsigned long timer = 0;
 
 void setup() {
   Serial.begin(9600);             // Porta serial para o arduino
   Serial1.begin(9600);            // Porta serial para o HC12
+  Serial2.begin(9600);            // Porta serial para o HC12
   lcd.begin(16,2);
 }
 
 void loop() {
-  int bytesSent = hc12Send();
-  int bytesRead = hc12Read();
-  lcdRefresh(bytesRead, bytesSent);
+
+  lcdRefresh();
 }
 
 
-void lcdRefresh(int bytesRead, int bytesSent){
+void lcdRefresh(){
   static long timer = 0;
 
   if(millis()>timer){
-
+    int read = hc12Read();
+    int sent = hc12Send();
     Serial.print("Recived:");
-    Serial.print(bytesRead);
+    Serial.print(read);
     Serial.print("  ||   Sent:");
-    Serial.println(bytesSent);
+    Serial.println(sent);
 
     lcd.clear();
     lcd.print("Recived:");
-    lcd.println(bytesRead);
+    lcd.println(read);
     lcd.setCursor(0,1);
     lcd.print("Sent:");
-    lcd.println(bytesSent);
+    lcd.println(sent);
 
     timer = millis()+1000;
   }
 }
 
 int hc12Read(){                   //RETORNA QUANTIDADE DE BITS QUE FOI ENVIADO
-  static int receivedBytes = 0;
-  while(Serial1.available()){
-    Serial1.read();
-    receivedBytes++;
+  int receivedBytes = 0;
+  byte read[4] = { 0, 0, 0, 0 };
+  while(Serial1.available() > 0) {
+  receivedBytes = Serial1.readBytes(read, 4);
+  }
+  int i = 0;
+  for(i;i < 4; i++) {
+    Serial.print("Byte: ");Serial.print(i);Serial.print(" - ");Serial.println(read[i]);
   }
   return receivedBytes;
 }
 
 
 int hc12Send(){                   //RETORNA A QUANTIDADE DE BITS QUE FOI RECIBIDA
-  static int sentBytes = 0;
-  static long timer = 0;
+  int sentBytes = 0;
 
-  if(millis()>timer){
-    sentBytes += Serial1.println(sentData);
-    timer = millis() + 1000;
-  }
+  sentBytes += Serial2.write(sentData);
+  sentBytes += Serial2.write(sentData);
+  sentBytes += Serial2.write(sentData);
+  sentBytes += Serial2.write(sentData);
+
   return  sentBytes;
 }
