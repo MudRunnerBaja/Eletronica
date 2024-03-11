a403730aaef9634c8adecf5ee5215d5478664ea5
Lucas <lucas.berton7@gmail.com>
2024-03-11 17:07:51 -0300

    Update testePrototipoMR23.ino

--- testes/testePrototipoMR23/testePrototipoMR23.ino
+++ testes/testePrototipoMR23/testePrototipoMR23.ino
@@ -10,6 +10,7 @@
 #define GPS_TX 8 // PINO TX UART GPS
 #define GPS_RX 9 // PINO RX UART GPS
 const unsigned long GPSBaud = 9600;
+
 //cartao sd
 #define SCKPIN 10
 #define TXPIN 11 // MOSI
@@ -22,59 +23,92 @@ const unsigned long GPSBaud = 9600;
 #define tx0 19  //MOSI
 #define cs0 17 
 
+int nvl_freio = 22, adc_divtensao = 26;
+float voltPerBit, tensao;
+float tensaoMaxBat = 13;//VOLTS
+
+
 TinyGPSPlus gps;
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 
 void setup() {
   Serial.begin(9600);  
   Serial.println("INCIALIZANDO");
+  pinMode(nvl_freio, INPUT);
+
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
-  for (size_t i = 0; i < 10; i++)
+
+  for (size_t i = 0; i < 10; i++)     //CONFIRMAÇÃO VISUAL QUE O PICO ESTA FUNCIONANDO
   {
     digitalWrite(LED_BUILTIN, HIGH);
     delay(100);
     digitalWrite(LED_BUILTIN, LOW);
     delay(100);
   }
-  
+  //SETANDO OS PINOS DO MODULO SD(SPI1)
   SPI1.setRX(RXPIN);
   SPI1.setTX(TXPIN);
   SPI1.setSCK(SCKPIN);
   SPI1.setCS(CSPIN);
   SPI1.begin(true);
-  
+  //INICIALIZANDO O CARTAO SD / VERIFICANDO A INCIALIZAÇÃO
   if(SD.begin(CSPIN, SPI1)){
-    digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(LED_BUILTIN, HIGH);    //LED ACESO = CARTAO SD INCIALIZADO
   }
 
+  //SETANDO PINOS DO CAN-BUS (SPI0)
   SPI.setSCK(sck0);
   SPI.setRX(rx0);
   SPI.setTX(tx0);
-
+  CAN.setPins(cs0);
+  //INCIALIANDO O CAN-BUS
   if (!CAN.begin(500E3)) {
     Serial.println("Starting CAN failed!");
     while (1);
   }
 
-  CAN.setPins(cs0);
-
-
+  //INICALIZA O SENSOR DE TEMPERATURA INFRAVERMELHO
   mlx.begin();
-  
+  voltPerBit = tensaoMaxBat / 1023;
+
+  //SETANDO PORTA DO GPS / INICIALIZANDO
   //Serial1.setTX(GPS_TX);
   //Serial1.setRX(GPS_RX);
   //Serial1.begin(GPSBaud);
+  
   Serial.println("INCILIZADO!");
 }
 
+bool fluido_baixo;
 void loop() {
-  Serial.println("LOOP!");
-  Serial.print("Ambient = "); Serial.print(mlx.readAmbientTempC());
-  Serial.print("*C\tObject = "); Serial.print(mlx.readObjectTempC()); Serial.println("*C");
-  Serial.println();
+  Serial.println("\nLOOP!");
+  int bits_divtensao = analogRead(adc_divtensao);
+  tensao = bits_divtensao * voltPerBit;
 
-  Serial.print("Sending packet ... ");
+  if(digitalRead(nvl_freio)){
+    fluido_baixo = true;
+  } else {
+    fluido_baixo = false;
+  }
+  Serial.print("FLUIDO DE FREIO: ");
+  
+  if(fluido_baixo){
+    Serial.println("BAIXO");
+  }else{
+    Serial.println("OK!");
+  }
+  
+  Serial.print("Tensao bat: ");
+  Serial.println(tensao);
+  
+  Serial.println("T. Ambiente\t T. Infrared");
+  Serial.print(mlx.readAmbientTempC());
+  Serial.print("*C\t       "); 
+  Serial.print(mlx.readObjectTempC()); 
+  Serial.println("*C");
+
+  Serial.print("CAN-BUS pacote padrao: ");
 
   CAN.beginPacket(0x12);
   CAN.write('h');
@@ -84,12 +118,12 @@ void loop() {
   CAN.write('o');
   CAN.endPacket();
 
-  Serial.println("done");
+  Serial.println("OK!");
 
   delay(200);
 
   // send extended packet: id is 29 bits, packet can contain up to 8 bytes of data
-  Serial.print("Sending extended packet ... ");
+  Serial.print("CAN-BUS pacote extendido: ");
 
   CAN.beginExtendedPacket(0xabcdef);
   CAN.write('w');
@@ -99,9 +133,7 @@ void loop() {
   CAN.write('d');
   CAN.endPacket();
 
-  Serial.println("done");
+  Serial.println("OK!");
 
-  delay(500);
-
-  
+  delay(500); 
 }
