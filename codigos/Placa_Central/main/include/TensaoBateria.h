7b0c83e0f9983d1f9f108e5bbca25b3cd51eb7de
UmBruxaoChamadoLesk <lukegalvao123@gmail.com>
2026-09-03 10:06:30 -0300

    pedal acelerador.h e tensao bateria.h convertido para RTOS

--- codigos/Placa_Central/main/include/TensaoBateria.h
+++ codigos/Placa_Central/main/include/TensaoBateria.h
@@ -5,8 +5,16 @@
 #ifndef _TENSAOBATERIA_H
 #define _TENSAOBATERIA_H
 
+#include <Arduino.h>
 #include "Setupable.h"
 #include "Constantes.h"
+
+#if defined(ARDUINO_ARCH_ESP32)
+#include "freertos/FreeRTOS.h"
+#include "freertos/semphr.h"
+#include "freertos/task.h"
+#define TENSAO_BATERIA_HAS_FREERTOS 1
+#endif
 
 class TensaoBateria
 {
@@ -16,8 +24,9 @@ public:
 
     double updateTensaoBateria()
     {
-        tensaoBateria;
-        return tensaoBateria;
+        const double leitura = (static_cast<double>(analogRead(DIV_TENSAO)) / 4095.0) * BATERIA_TENSAO_MAX;
+        setTensaoBateria(leitura);
+        return leitura;
     }
 
     bool Debug()
@@ -27,8 +36,14 @@ public:
 
     double getTensaoBateria()
     {
-        tensaoBateria = (analogRead(A0) / 1023) * 13.2;
-        return tensaoBateria;
+#if !TENSAO_BATERIA_HAS_FREERTOS
+        return updateTensaoBateria();
+#else
+        lock();
+        const double leitura = tensaoBateria;
+        unlock();
+        return leitura;
+#endif
     }
 
     TensaoBateria(TensaoBateria &outro) = delete;
@@ -39,10 +54,51 @@ public:
         {
             instance = this;
         }
+
+#if TENSAO_BATERIA_HAS_FREERTOS
+        mutex = xSemaphoreCreateMutex();
+        xTaskCreate(taskAtualizacao, "tensao-bateria", 2048, this, 1, &task);
+#endif
     }
 
 private:
-    double tensaoBateria;
+    double tensaoBateria = 0.0;
+
+#if TENSAO_BATERIA_HAS_FREERTOS
+    SemaphoreHandle_t mutex;
+    TaskHandle_t task;
+
+    static void taskAtualizacao(void *contexto)
+    {
+        auto *sensor = static_cast<TensaoBateria *>(contexto);
+
+        for (;;)
+        {
+            sensor->updateTensaoBateria();
+            vTaskDelay(pdMS_TO_TICKS(INTERVALO_TIMER_MS));
+        }
+    }
+
+    void lock()
+    {
+        xSemaphoreTake(mutex, portMAX_DELAY);
+    }
+
+    void unlock()
+    {
+        xSemaphoreGive(mutex);
+    }
+#else
+    void lock() {}
+    void unlock() {}
+#endif
+
+    void setTensaoBateria(double valor)
+    {
+        lock();
+        tensaoBateria = valor;
+        unlock();
+    }
 };
 
 TensaoBateria *TensaoBateria::instance{nullptr};
